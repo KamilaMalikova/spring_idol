a4f14fd87b897357ff64fd7e880d8390fed2b251
KamilaMalikova <kamilamalikova173@gmail.com>
2021-08-20 04:10:47 -0700

    Autowired

--- src/main/java/com/example/spring_try/models/extra/impl/Guitar.java
+++ src/main/java/com/example/spring_try/models/extra/impl/Guitar.java
@@ -1,7 +1,11 @@
 package com.example.spring_try.models.extra.impl;
 
+import com.example.spring_try.annotations.StringedInstrument;
+import com.example.spring_try.annotations.Strummed;
 import com.example.spring_try.models.extra.Instrument;
 
+@StringedInstrument
+@Strummed
 public class Guitar implements Instrument {
     @Override
     public void play() {
--- src/main/java/com/example/spring_try/models/impl/Instrumentalist.java
+++ src/main/java/com/example/spring_try/models/impl/Instrumentalist.java
@@ -1,12 +1,22 @@
 package com.example.spring_try.models.impl;
 
+import com.example.spring_try.annotations.StringedInstrument;
+import com.example.spring_try.annotations.Strummed;
 import com.example.spring_try.exceptions.PerformanceException;
 import com.example.spring_try.models.Performer;
 import com.example.spring_try.models.extra.Instrument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 public class Instrumentalist implements Performer {
+    @Autowired
+    @StringedInstrument
+    @Strummed
     private Instrument instrument;
     private String song;
+
     public Instrumentalist() {
     }
 
